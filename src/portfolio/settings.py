18fb16e436ed449f925cd37452cb59feb982fead
CodeANGEL <code.angel.091@gmail.com>
2023-05-25 22:37:48 +0000

    toujours la config pour la production

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -12,25 +12,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# environ.Environ.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-# SECRET_KEY = "django-insecure-aau3)_3gzofb8ch(wdoc+dw!6+4zhc6i_tarly*-yrvs#1pvj+"
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-aau3)_3gzofb8ch(wdoc+dw!6+4zhc6i_tarly*-yrvs#1pvj+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config("DEBUG")
+# DEBUG = config('DEBUG')
+DEBUG = True
 # DEBUG = False
 
-ALLOWED_HOSTS = list(config("ALLOWED_HOSTS"))
+ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ["codeangel.pythonanywhere.com"]
 
 
@@ -124,9 +126,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 MEDIA_URL = '/media/'
 
