8af1aede71fe89d428bab94ff209757fb70ac700
CodeANGEL <code.angel.091@gmail.com>
2023-05-26 12:20:47 +0000

    sur les css

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -125,7 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_DIRS = [
+    BASE_DIR / "/static/"
+]
 
 MEDIA_URL = '/media/'
 
