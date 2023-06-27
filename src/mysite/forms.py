99e550ff5837b063b190a1e54442f359dfc4e2e5
CodeANGEL <code.angel.091@gmail.com>
2023-06-27 03:21:56 +0000

    je sais pas

--- src/mysite/forms.py
+++ src/mysite/forms.py
@@ -1,8 +1,5 @@
 from django import forms
 from .models import Contact, CommanderService
-from crispy_forms.bootstrap import StrictButton
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset
 
 
 class ContactForm(forms.ModelForm):
--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'mysite',
     "blog",
     "accounts",
-    "crispy_forms"
 ]
 
 MIDDLEWARE = [
