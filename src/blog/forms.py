574c9a960fd4d622c03fb4d7eb8b8ad06ba74392
CodeANGEL <code.angel.091@gmail.com>
2023-06-27 02:21:49 +0000

    gestion de la redaction des articles

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -6,4 +6,7 @@ class ArticleForm(forms.ModelForm):
 	class Meta:
 		model = Articles
 		fields = ("titre", "categorie", "content", "img")
+		widgets = {
+			'content': forms.Textarea(attrs={'class': 'content'})
+		}
 
